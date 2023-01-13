13e1a63d4a32c0c93fd5b16bf32b47d24aba77ee
João Vitor <joaovitorojhhio12@gmail.com>
2023-01-13 19:25:56 -0300

    Exercicios-Alura

---
+++ Python-Começando com a Linguagem/Ex-1-Entendo-o-Sep/Main.py
@@ -0,0 +1,6 @@
+#Entendendo o Sep do print, basicamente ele define o que vai estar entre as variaveis do print
+dia = 15
+mes = 10
+ano = 2015
+
+print(dia,mes,ano, sep="/")
---
+++ Python-Começando com a Linguagem/Ex-2-Jogos/forca.py
@@ -0,0 +1,9 @@
+from os import system
+def jogar():
+    system('cls')
+    print('*'*40)
+    print('Bem Vindo no jogo da Forca')
+    print('*'*40)
+
+if (__name__ == '__main__'):
+    jogar()
